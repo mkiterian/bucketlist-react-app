90dc6eb805002ee48347841b4f9e3a4c344a7c15
mkiterian <hmukite@gmail.com>
2017-09-15 06:58:15 +0300

    [Fix] Move get bucketlist api call to helper file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import RegisterForm from './forms/RegisterForm';
 import LoginForm from './forms/LoginForm';
-
 import Home from './components/Home';
-
 import BucketlistsView from './components/BucketlistsView';
 import BucketlistView from './components/BucketlistView';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { blueGrey800, blueGrey500 } from 'material-ui/styles/colors';
-
 import { Route, Switch } from 'react-router-dom';
-
 import ROOT_URL from './constants';
-import axios from 'axios';
+import helpers from './helpers';
 
 
 const muiTheme = getMuiTheme({
@@ -42,24 +35,6 @@ class App extends Component {
     };
   }
 
-  getBucketlists() {
-    var bucketlists = {};
-    axios({
-      method: 'get',
-      url: ROOT_URL + '/api/v1/bucketlists',
-      headers: {
-        "Authorization": `JWT ${window.sessionStorage.accessToken}`,
-      }
-    }).then(response => {
-      bucketlists = response.data.bucketlists;
-      this.setState({
-        bucketlists: bucketlists,
-      })
-    }).catch(error => {
-      return error;
-    });
-  }
-
   addBucketlist(bucketlist) {
     var timestamp = (new Date()).getTime();
     this.state.bucketlists['bckt-' + timestamp] = bucketlist;
@@ -78,7 +53,7 @@ class App extends Component {
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegisterForm} />
             <Route exact path="/bucketlists/" render={()=>
-            <BucketlistsView getBucketlists={this.getBucketlists.bind(this)} 
+            <BucketlistsView getBucketlists={helpers.getBucketlists.bind(this)} 
                              addBucketlist = {this.addBucketlist.bind(this)}
                              bucketlists = {this.state.bucketlists}
                              bucketlist = {this.state.bucketlist}
