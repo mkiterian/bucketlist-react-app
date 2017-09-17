19c593f7131914a7d67d81fe1ed97700551fb6aa
mkiterian <hmukite@gmail.com>
2017-09-17 12:00:11 +0300

    Wrap switch in router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import BucketlistView from './components/BucketlistView';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { blueGrey800, blueGrey500 } from 'material-ui/styles/colors';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import helpers from './helpers';
 
 
@@ -46,7 +46,7 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
-          
+          <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={LoginForm} />
@@ -60,6 +60,7 @@ class App extends Component {
             <Route exact path="/bucketlists/:id/items" component={ BucketlistView } name=""/>
             
           </Switch>
+          </BrowserRouter>
         </div>
       </MuiThemeProvider>
     );
